2b1ba7e2e52595fff50ba99c5b275e65866d7e98
Oleksiy Asiuchenko <asucenkooleksij@gmail.com>
2023-09-07 18:24:35 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import "./sass/main.scss";
+
+class main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentNumber: props.initialValue || 0,
+    };
+  }
+  minus = () => {
+    this.setState((prevState) => ({
+      currentNumber: prevState.currentNumber - 1,
+    }));
+  };
+  plus = () => {
+    this.setState((prevState) => ({
+      currentNumber: prevState.currentNumber + 1,
+    }));
+  };
+  result = () => {
+    this.setState({
+      currentNumber: 0,
+    });
+  };
+  render() {
+    const { currentNumber } = this.state;
+    return (
+      <div className="main">
+        <div className="main__box-clicker box-clicker">
+          <button
+            className="box-clicker__minus"
+            id="clickMinus"
+            onClick={this.minus}
+          >
+            -
+          </button>
+          <div className="box-clicker__inner">
+            <p className="box-clicker__number" id="showNumber">
+              {currentNumber}
+            </p>
+            <button
+              className="box-clickerbtn-reset"
+              id="btnReset"
+              onClick={this.result}
+            >
+              Reset
+            </button>
+          </div>
+          <button
+            className="box-clicker__plus"
+            id="clickPlus"
+            onClick={this.plus}
+          >
+            +
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+export default main;
